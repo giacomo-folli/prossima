9909911373737183a0f3d9d0cec6971b6a1d15aa
Giacomo <giacomo.folli@studenti.unipr.it>
2026-09-25 19:37:41 +0200

    fix: remove posthog tracker from dev env

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,21 +1,33 @@
-import posthog from 'posthog-js';
-import { PUBLIC_POSTHOG_PROJECT_TOKEN, PUBLIC_POSTHOG_HOST } from '$env/static/public';
-import type { HandleClientError } from '@sveltejs/kit';
+import posthog from "posthog-js";
+import {
+  PUBLIC_POSTHOG_PROJECT_TOKEN,
+  PUBLIC_POSTHOG_HOST,
+} from "$env/static/public";
+import type { HandleClientError } from "@sveltejs/kit";
+import { dev } from "$app/environment";
 
 export async function init() {
-	posthog.init(PUBLIC_POSTHOG_PROJECT_TOKEN, {
-		api_host: PUBLIC_POSTHOG_HOST,
-		ui_host: 'https://us.posthog.com',
-		defaults: '2026-01-30',
-		capture_exceptions: true
-	});
+  if (!dev) {
+    posthog.init(PUBLIC_POSTHOG_PROJECT_TOKEN, {
+      api_host: PUBLIC_POSTHOG_HOST,
+      ui_host: "https://us.posthog.com",
+      defaults: "2026-01-30",
+      capture_exceptions: true,
+    });
+  }
 }
 
-export const handleError: HandleClientError = async ({ error, status, message }) => {
-	posthog.captureException(error);
+export const handleError: HandleClientError = async ({
+  error,
+  status,
+  message,
+}) => {
+  if (!dev) {
+    posthog.captureException(error);
+  }
 
-	return {
-		message,
-		status
-	};
+  return {
+    message,
+    status,
+  };
 };
